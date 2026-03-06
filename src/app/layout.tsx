7015e923a65262224eb7dde891dc3df091616f8e
saddiq musa <saddiq0musa@gmail.com>
2026-03-07 00:33:56 +0300

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -46,6 +47,23 @@ export default function RootLayout({
 							{children}
 							<Toaster />
 							<Analytics />
+							{process.env.NEXT_PUBLIC_GA_ID && (
+								<>
+									<Script
+										src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+										strategy="afterInteractive"
+									/>
+									<Script id="google-analytics" strategy="afterInteractive">
+										{`
+											window.dataLayer = window.dataLayer || [];
+											function gtag(){dataLayer.push(arguments);}
+											gtag('js', new Date());
+
+											gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+										`}
+									</Script>
+								</>
+							)}
 						</MotionProvider>
 					</QueryProvider>
 				</ThemeProvider>
